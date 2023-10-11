0f03253479041a4e03ec7ccf24f9482f31e53d32
Anshuman Dubey <112381325+Anshuman1609@users.noreply.github.com>
2023-10-11 14:09:42 +0530

    commit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -12,7 +12,7 @@ text-lg text-cyan flex items-center
      "
     >
       <img src={logoSvg} alt="CryptoBucks" />
-      <span>CryptoHunters</span>
+      <span>RT-Crypto</span>
     </Link>
   );
 };
